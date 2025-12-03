642cb3ef4e6a7b6970bcfddf72c5c6d91763972c
Wouter Jehee <wouter@wouterjehee.com>
2025-12-03 10:24:14 +0100

    day 3 part 1

--- src/day03.rs
+++ src/day03.rs
@@ -1,9 +1,26 @@
 use std::fs::read_to_string;
+use std::cmp::max;
 
-pub fn solve(path: &str, part2: bool) -> i64 {
+pub fn solve(path: &str, part2: bool) -> i32 {
+    let mut result = 0;
     for line in read_to_string(path).unwrap().lines() {
+        let mut left = -10;
+        let mut joltage = -10;
+        for (i, c) in line.chars().enumerate() {
+            let c = c.to_digit(10).expect("invalid digit") as i32;
+            if c > left {
+                left = c;
+                let mut right = -10;
+                for cr in line.chars().skip(i+1) {
+                    let cr = cr.to_digit(10).expect("invalid digit") as i32;
+                    right = max(right, cr);
+                }
+                joltage = max(joltage, left * 10 + right);
+            }
+        }
+        result += joltage;
     }
-    0
+    result
 }
 
 #[cfg(test)]
@@ -12,26 +29,26 @@ mod tests {
 
     #[test]
     fn test_example_p1() {
-        let result = solve("examples/dayxx.txt", false);
-        assert_eq!(result, -1);
+        let result = solve("examples/day03.txt", false);
+        assert_eq!(result, 357);
     }
 
     #[test]
     fn test_input_p1() {
-        let result = solve("inputs/dayxx.txt", false);
-        assert_eq!(result, -1);
+        let result = solve("inputs/day03.txt", false);
+        assert_eq!(result, 17100);
     }
 
     #[test]
     fn test_example_p2() {
-        let result = solve("examples/dayxx.txt", true);
-        assert_eq!(result, -1);
+        let result = solve("examples/day03.txt", true);
+        assert_eq!(result, 0);
     }
 
     #[test]
     fn test_input_p2() {
-        let result = solve("inputs/dayxx.txt", true);
-        assert_eq!(result, -1);
+        let result = solve("inputs/day03.txt", true);
+        assert_eq!(result, 0);
     }
 }
 
