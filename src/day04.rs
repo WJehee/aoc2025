b4ea526b9e43ba433f12bdb117e1b05daefc28ce
Wouter Jehee <wouter@wouterjehee.com>
2025-12-07 13:41:49 +0100

    solve day 4 part 1

---
+++ src/day04.rs
@@ -0,0 +1,81 @@
+use std::fs::read_to_string;
+
+pub fn solve(path: &str, part2: bool) -> i64 {
+    let mut grid: Vec<Vec<char>> = Vec::new();
+    for line in read_to_string(path).unwrap().lines() {
+        let mut row = Vec::new();
+        for c in line.chars() {
+            row.push(c);
+        }
+        grid.push(row);
+    }
+    let mut result = 0;
+    for (i, _row) in grid.iter().enumerate() {
+        for (j, item) in _row.iter().enumerate() {
+            let mut roll_count = -1;
+            if *item == '@' {
+                // println!("{},{}", i, j);
+                roll_count = 0;
+                let mut ids = vec![i, i+1];
+                if i != 0 {
+                    ids.insert(0, i-1);
+                }
+                let rows: Vec<_> = ids.iter()
+                    .filter_map(|&x| grid.get(x))
+                    .collect();
+                for row in &rows {
+                    let mut ids = vec![j, j+1];
+                    if j != 0 {
+                        ids.insert(0, j-1);
+                    }
+                    let items: Vec<_> = ids.iter()
+                        .filter_map(|&x| row.get(x))
+                        .collect();
+
+                    // println!("{:?}", items);
+                    for char in items {
+                        if *char == '@' { roll_count += 1; }
+                    }
+                }
+                // println!();
+            }
+            if roll_count > 0 && roll_count <= 4 { 
+                result += 1;
+                print!("x");
+            } 
+            print!("{}", item);
+        }
+        println!();
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example_p1() {
+        let result = solve("examples/day04.txt", false);
+        assert_eq!(result, 13);
+    }
+
+    #[test]
+    fn test_input_p1() {
+        let result = solve("inputs/day04.txt", false);
+        assert_eq!(result, 1349);
+    }
+
+    // #[test]
+    // fn test_example_p2() {
+    //     let result = solve("examples/day04.txt", true);
+    //     assert_eq!(result, -1);
+    // }
+    //
+    // #[test]
+    // fn test_input_p2() {
+    //     let result = solve("inputs/day04.txt", true);
+    //     assert_eq!(result, -1);
+    // }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 fn main() {
     day03::solve("examples/day03.txt", false);
